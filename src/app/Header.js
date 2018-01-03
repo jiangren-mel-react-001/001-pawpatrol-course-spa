3ea9e3444c42a9d67624d5e63807edf2eadfe6b9
tonychangcheng <cheng.chang2@pactera.com>
2018-01-03 11:44:21 +1100

    add react-bootstrap nav

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  Navbar, NavItem, Nav
+} from 'react-bootstrap';
 
 export default () => (
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/course">Courses</Link></li>
-        <li><Link to="/teacher">Teachers</Link></li>
-      </ul>
+  <Navbar inverse collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <Link to="/">Jiangren</Link>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1}>
+          <Link to="/course">Courses</Link>
+        </NavItem>
+        <NavItem eventKey={2}>
+          <Link to="/teacher">Teachers</Link>
+        </NavItem>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1}>
+          <Link to="/course">Courses</Link>
+        </NavItem>
+        <NavItem eventKey={2}>
+          <Link to="/teacher">Teachers</Link>
+        </NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 );
